af85c2c87404f6ef70b2a8539829d89af1fdc969
Alisson-Johnson <joalisso@sheridancollege.ca>
2024-09-13 10:03:45 -0400

    Initial Commit -just added a comment

---
+++ src/myfirstjavaproject/MyFirstJavaProject.java
@@ -0,0 +1,17 @@
+package myfirstjavaproject;
+
+/**
+ *
+ * @author johns
+ */
+public class MyFirstJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        System.out.println("Hello"); //Making a change
+    }
+    
+}
